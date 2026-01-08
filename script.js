2d49906493f8583d109178c737472d038c761bc8
saifxx1101-hub <saifxx1101@gmail.com>
2026-01-08 19:54:23 +0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Birthday page loaded");
+const images = [
+  "Birthday1.jpg",
+  "Birthday2.jpg",
+  "Birthday3.jpg"
+];
+
+let index = 0;
+const slide = document.getElementById("slide");
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  slide.src = images[index];
+}, 2500);
+
+function playMusic() {
+  document.getElementById("music").play();
+}
