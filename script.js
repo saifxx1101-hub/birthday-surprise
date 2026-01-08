b833752bb95bcec21f768000dc06dd664a1b32c9
saifxx1101-hub <saifxx1101@gmail.com>
2026-01-08 20:04:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,9 @@ if (userName) {
 }
 
 // IMAGE SLIDER
-const images = ["Birthday1.jpg", "Birthday2.jpg", "Birthday3.jpg"];
 let index = 0;
 const slide = document.getElementById("slide");
 
-setInterval(() => {
-  index = (index + 1) % images.length;
-  slide.src = images[index];
-}, 2500);
 
 // MUSIC
 function playMusic() {
